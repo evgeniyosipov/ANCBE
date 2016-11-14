24d7518153f85dfeb6bbd65919209526fda1c9b9
Evgeniy Osipov <osipoveg1990@gmail.com>
2016-11-14 22:39:01 +0300

    Added gulp.js - configured for essential copy/delete actions for third party css/js files. Also changed bower libs location.

---
+++ src/ANCBE/gulpfile.js
@@ -0,0 +1,26 @@
+﻿/// <binding BeforeBuild='default' Clean='clean' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    del = require('del');
+
+gulp.task('clean', function () {
+    return del(['wwwroot/css/third_party', 'wwwroot/js/third_party']);
+});
+
+gulp.task('copyCss', function () {
+    gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
+        .pipe(gulp.dest('wwwroot/css/third_party'));
+});
+
+gulp.task('copyJs', function () {
+    gulp.src(['bower_components/jquery/dist/jquery.min.js',
+              'bower_components/bootstrap/dist/js/bootstrap.min.js',
+              'bower_components/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest('wwwroot/js/third_party'));
+});
+
+gulp.task('default', ['clean', 'copyCss', 'copyJs']);

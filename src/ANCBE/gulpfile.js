fccaa8797b4e91efc7cd3f55faebc90a8b779e97
Evgeniy Osipov <osipoveg1990@gmail.com>
2016-11-15 21:36:33 +0300

    Edited .gitignore and gulpfile.js for exclude third-party css and js from git

--- src/ANCBE/gulpfile.js
+++ src/ANCBE/gulpfile.js
@@ -8,19 +8,19 @@ var gulp = require('gulp'),
     del = require('del');
 
 gulp.task('clean', function () {
-    return del(['wwwroot/css/third_party', 'wwwroot/js/third_party']);
+    return del(['wwwroot/css/third-party', 'wwwroot/js/third-party']);
 });
 
 gulp.task('copyCss', function () {
     gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
-        .pipe(gulp.dest('wwwroot/css/third_party'));
+        .pipe(gulp.dest('wwwroot/css/third-party'));
 });
 
 gulp.task('copyJs', function () {
     gulp.src(['bower_components/jquery/dist/jquery.min.js',
               'bower_components/bootstrap/dist/js/bootstrap.min.js',
               'bower_components/tether/dist/js/tether.min.js'])
-        .pipe(gulp.dest('wwwroot/js/third_party'));
+        .pipe(gulp.dest('wwwroot/js/third-party'));
 });
 
 gulp.task('default', ['clean', 'copyCss', 'copyJs']);

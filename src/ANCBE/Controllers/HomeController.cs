5464a36a6aacb1b11b70c3b46496238d659ca207
Evgeniy Osipov <osipoveg1990@gmail.com>
2016-11-21 20:59:01 +0300

    Replaced @ViewBag properties by Models/Post.cs

--- src/ANCBE/Controllers/HomeController.cs
+++ src/ANCBE/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ANCBE.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,11 +19,14 @@ namespace ANCBE.Controllers
 
         public IActionResult Post(long id)
         {
-            ViewBag.Title = "Incredible Post";
-            ViewBag.Posted = DateTime.Now;
-            ViewBag.Auhor = "Evgeniy";
-            ViewBag.Body = "A lot of text. This is realy awesome post... with pictures and sounds. Believe me, this is the coolest post of all time ;)";
-            return View();
+            Post post = new Post();
+
+            post.Title = "Incredible Post";
+            post.PostedDate = DateTime.Now;
+            post.Auhor = "Evgeniy";
+            post.Body = "A lot of text. This is realy awesome post... with pictures and sounds. Believe me, this is the coolest post of all time ;)";
+
+            return View(post);
         }
     }
 }
---
+++ src/ANCBE/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANCBE.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public DateTime PostedDate { get; set; }
+        public string Auhor { get; set; }
+        public string Body { get; set; }
+    }
+}

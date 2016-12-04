dc2726680892e852f4fee45d2ea92d0f80983708
Evgeniy Osipov <osipoveg1990@gmail.com>
2016-12-04 21:56:45 +0300

    Reused application features with view components

---
+++ src/ANCBE/ViewComponents/ArchivedPostsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using ANCBE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANCBE.ViewComponents
+{
+    [ViewComponent]
+    public class ArchivedPostsViewComponent : ViewComponent
+    {
+        private readonly BlogDataContext _db;
+
+        public ArchivedPostsViewComponent(BlogDataContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var archivedPosts = _db.GetArchivedPosts().ToArray();
+            return View(archivedPosts);
+        }
+    }
+}
